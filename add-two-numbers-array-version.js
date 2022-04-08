5a6a8956dbf299730c8c5d486f983607c77bfe0e
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-04-08 09:38:24 -0700

    Alternate file with solution for #2 but with arrays

--- add-two-numbers-array-version.js
+++ add-two-numbers-array-version.js
@@ -1,22 +1,14 @@
-/*
- * @lc app=leetcode id=2 lang=javascript
- *
- * [2] Add Two Numbers
- */
+// Examples
+// Input: l1 = [2,4,3], l2 = [5,6,4]
+// Output: [7,0,8]
+// Explanation: 342 + 465 = 807.
 
-// @lc code=start
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
+// Input: l1 = [0], l2 = [0]
+// Output: [0]
+
+// Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+// Output: [8,9,9,9,0,0,0,1]
+
 var addTwoNumbers = function (l1, l2) {
     const stringifiedL1 = l1.join("");
     const stringifiedL2 = l2.join("");
@@ -39,4 +31,3 @@ var addTwoNumbers = function (l1, l2) {
 
     return reversedTotalArray;
 };
-// @lc code=end
