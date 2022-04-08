1c9f05074eabbef8829b25656c0ad793f8a8ce24
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-04-08 10:18:41 -0700

    Add better solution to #9

--- 9.palindrome-number.js
+++ 9.palindrome-number.js
@@ -30,4 +30,16 @@ var isPalindrome = function (x) {
         return false;
     }
 };
+
 // @lc code=end
+
+// BEST SOLUTION
+// Use a varibale to reverse number then check with given number
+
+// var isPalindrome = function(x) {
+//   if (x < 0) return false
+
+//   let rev = 0
+//   for(let i = x; i >= 1; i = Math.floor(i/10)) rev = rev*10 + i%10
+//   return rev === x
+// };
