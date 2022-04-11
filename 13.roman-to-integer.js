3cbde40561658621ca716e75e5219af71bf7b8e4
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-04-11 16:23:20 -0700

    Answer #13

--- 13.roman-to-integer.js
+++ 13.roman-to-integer.js
@@ -9,8 +9,57 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
-    
+var romanToInt = function (s) {
+    let currentNum = 0;
+    let prevNum = 0;
+    let convertedToInt = 0;
+
+    let stringToArr = s.split("");
+
+    stringToArr.forEach((romanNumeral) => {
+        prevNum = currentNum;
+        currentNum = 0;
+        switch (romanNumeral) {
+            case "I":
+                currentNum += 1;
+                break;
+            case "V":
+                currentNum += 5;
+                break;
+            case "X":
+                currentNum += 10;
+                break;
+            case "L":
+                currentNum += 50;
+                break;
+            case "C":
+                currentNum += 100;
+                break;
+            case "D":
+                currentNum += 500;
+                break;
+            case "M":
+                currentNum += 1000;
+                break;
+        }
+
+        if (prevNum === 1 && currentNum === 5) {
+            convertedToInt += 3;
+        } else if (prevNum === 1 && currentNum === 10) {
+            convertedToInt += 8;
+        } else if (prevNum === 10 && currentNum === 50) {
+            convertedToInt += 30;
+        } else if (prevNum === 10 && currentNum === 100) {
+            convertedToInt += 80;
+        } else if (prevNum === 100 && currentNum === 500) {
+            convertedToInt += 300;
+        } else if (prevNum === 100 && currentNum === 1000) {
+            convertedToInt += 800;
+        } else {
+            convertedToInt += currentNum;
+        }
+    });
+
+    return convertedToInt;
 };
 // @lc code=end
-
