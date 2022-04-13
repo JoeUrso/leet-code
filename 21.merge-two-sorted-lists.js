8156b53bfda269d2a49f664a41d3bb9842743eff
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-04-13 11:53:13 -0700

    Cannot solve #21, add solutions

--- 21.merge-two-sorted-lists.js
+++ 21.merge-two-sorted-lists.js
@@ -17,8 +17,34 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function(list1, list2) {
-    
-};
+var mergeTwoLists = function (list1, list2) {};
 // @lc code=end
 
+// SOLUTIONS
+// var mergeTwoLists = function (l1, l2) {
+//     if (!l1 || !l2) return l1 ? l1 : l2;
+//     if (l1.val < l2.val) {
+//         l1.next = mergeTwoLists(l1.next, l2);
+//         return l1;
+//     } else {
+//         l2.next = mergeTwoLists(l1, l2.next);
+//         return l2;
+//     }
+// };
+
+// const mergeTwoLists = function (l1, l2) {
+//     const head = new ListNode(null);
+//     let curr = head;
+//     while (l1 && l2) {
+//         if (l1.val < l2.val) {
+//             curr.next = l1;
+//             l1 = l1.next;
+//         } else {
+//             curr.next = l2;
+//             l2 = l2.next;
+//         }
+//         curr = curr.next;
+//     }
+//     curr.next = l1 || l2;
+//     return head.next;
+// };
