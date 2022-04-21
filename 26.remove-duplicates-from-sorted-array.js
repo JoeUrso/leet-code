cef6d6c1680f7f1e9a774e40d4095f6ab60e2c70
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-04-20 19:08:37 -0700

    Came up with a few ideas that arnt working yet

--- 26.remove-duplicates-from-sorted-array.js
+++ 26.remove-duplicates-from-sorted-array.js
@@ -9,8 +9,57 @@
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function(nums) {
-    
+var removeDuplicates = function (nums) {
+    nums.reduce((previousValue, currentValue) => {
+        if (previousValue === currentValue) {
+            nums.shift(nums[0]);
+            if (nums.pop() === nums[0]) {
+                nums.push(currentValue);
+            }
+        } else {
+            nums.shift(nums[0]);
+        }
+    });
+
+    return nums;
 };
 // @lc code=end
 
+// Maybe?
+
+// nums.reduce((previousValue, currentValue) => {
+//     let itemsToRemove = 0
+//     console.log(previousValue)
+//     if (previousValue === currentValue) {
+// //             nums.shift(nums[0]);
+//         itemsToRemove++
+//         console.log(nums)
+//     } else if (previousValue !== currentValue) {
+//         itemsToRemove++
+//         nums.push(nums[0])
+//         console.log(nums)
+//     }
+
+//     for (i = 0; i <= itemsToRemove; i++) {
+//         nums.shift[0]
+//     }
+
+// Other idea?
+// var removeDuplicates = function (nums) {
+
+//     for (i = 0; i < nums.length; i++) {
+//         let previous = nums[0]
+//     let current = nums[1]
+
+//         if (previous === current) {
+//             nums.shift(nums[0]);
+//             if (previous === nums[0]) {
+//                 nums.push(current);
+//         } else {
+//             nums.shift(nums[0]);
+//     };
+//     };
+// }
+
+//     return nums;
+// };
