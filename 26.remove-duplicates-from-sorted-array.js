b5460b4e9ae517a3dce208b37abb683b2a37e71a
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-04-22 15:05:10 -0700

    Answer #26

--- 26.remove-duplicates-from-sorted-array.js
+++ 26.remove-duplicates-from-sorted-array.js
@@ -10,71 +10,26 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-    nums.reduce((previousValue, currentValue) => {
-        if (previousValue === currentValue) {
-            nums.shift(nums[0]);
-            if (nums.pop() === nums[0]) {
-                nums.push(currentValue);
-            }
-        } else {
-            nums.shift(nums[0]);
+    const initialNumsLength = nums.length;
+    let itemsToRemove = 0;
+
+    for (i = 0; i < initialNumsLength; i++) {
+        let current = nums[i];
+        let next = nums[i + 1];
+
+        if (next === current) {
+            itemsToRemove++;
+        } else if (next !== current) {
+            itemsToRemove++;
+            nums.push(current);
         }
-    });
+    }
+
+    while (itemsToRemove > 0) {
+        nums.shift();
+        itemsToRemove--;
+    }
 
     return nums;
 };
 // @lc code=end
-
-// Maybe?
-
-// nums.reduce((previousValue, currentValue) => {
-//     let itemsToRemove = 0
-//     console.log(previousValue)
-//     if (previousValue === currentValue) {
-// //             nums.shift(nums[0]);
-//         itemsToRemove++
-//         console.log(nums)
-//     } else if (previousValue !== currentValue) {
-//         itemsToRemove++
-//         nums.push(nums[0])
-//         console.log(nums)
-//     }
-
-//     for (i = 0; i <= itemsToRemove; i++) {
-//         nums.shift[0]
-//     }
-
-// Other ideas?
-// var removeDuplicates = function (nums) {
-
-//     for (i = 0; i < nums.length; i++) {
-//         let previous = nums[0]
-//     let current = nums[1]
-
-//         if (previous === current) {
-//             nums.shift(nums[0]);
-//             if (previous === nums[0]) {
-//                 nums.push(current);
-//         } else {
-//             nums.shift(nums[0]);
-//     };
-//     };
-// }
-
-//     return nums;
-// };
-
-// var removeDuplicates = function (nums) {
-//     nums.reduce((previousValue, currentValue) => {
-//         if (previousValue === currentValue) {
-//             nums.shift(nums[0]);
-//             console.log(nums)
-//         } else if (previousValue !== currentValue) {
-//             let numToPush = nums.shift(nums[0]);
-//             nums.push(numToPush)
-//             console.log(nums)
-//         }
-//     });
-
-//     return nums;
-// };
