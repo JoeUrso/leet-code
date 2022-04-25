beef6412b6fee6457aad6c2c2481608280e0ad6b
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-04-25 14:53:11 -0700

    Solid solution for #28 but need to figure out edge cases

--- 28.implement-str-str.js
+++ 28.implement-str-str.js
@@ -10,8 +10,28 @@
  * @param {string} needle
  * @return {number}
  */
-var strStr = function(haystack, needle) {
-    
+var strStr = function (haystack, needle) {
+    let indexOccurrence = -1;
+
+    if (!needle) {
+        return 0;
+    }
+
+    for (i = 0; i < haystack.length; i++) {
+        if (haystack[i] === needle[0]) {
+            if (haystack[i] !== haystack[i - 1]) {
+                indexOccurrence = i;
+            }
+            for (j = 1; j < needle.length; j++) {
+                i++;
+
+                if (haystack[i] !== needle[j]) {
+                    indexOccurrence = -1;
+                }
+            }
+        }
+    }
+
+    return indexOccurrence;
 };
 // @lc code=end
-
