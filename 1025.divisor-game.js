48a95b674347ec359968034d0782dc070fa6f6bc
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-06-21 18:05:24 -0700

    Solved #1025

---
+++ 1025.divisor-game.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1025 lang=javascript
+ *
+ * [1025] Divisor Game
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var divisorGame = function (n) {
+    return n % 2 === 0;
+};
+// @lc code=end
