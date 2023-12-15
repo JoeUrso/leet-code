87561dda6323a31e927d95519dccfa4590c7c380
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2023-12-15 09:59:21 -0500

    complete leetcode 872

---
+++ 872.leaf-similar-tress.js
@@ -0,0 +1,58 @@
+// Consider all the leaves of a binary tree, from left to right order, the values of those leaves form a leaf value sequence.
+
+// For example, in the given tree above, the leaf value sequence is (6, 7, 4, 9, 8).
+
+// Two binary trees are considered leaf-similar if their leaf value sequence is the same.
+
+// Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar.
+
+// Example 1:
+
+// Input: root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
+// Output: true
+// Example 2:
+
+// Input: root1 = [1,2,3], root2 = [1,3,2]
+// Output: false
+
+// Constraints:
+
+// The number of nodes in each tree will be in the range [1, 200].
+// Both of the given trees will have values in the range [0, 200].
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+var leafSimilar = function (root1, root2) {
+    const leafValues1 = [];
+    const leafValues2 = [];
+
+    function dfs(node, leafValues) {
+        if (!node) return;
+        if (!node.left && !node.right) {
+            leafValues.push(node.val);
+        } else {
+            dfs(node.left, leafValues);
+            dfs(node.right, leafValues);
+        }
+    }
+
+    dfs(root1, leafValues1);
+    dfs(root2, leafValues2);
+
+    return (
+        leafValues1.length === leafValues2.length &&
+        leafValues1.every((val, index) => val === leafValues2[index])
+    );
+};
